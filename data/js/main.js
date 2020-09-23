5b1beb3f636876a3ec5c28728090c8756c4b2d14
Dakumisu <56684844+Dakumisu@users.noreply.github.com>
2020-09-23 09:45:31 +0200

    la zone

--- data/js/main.js
+++ data/js/main.js
@@ -44,4 +44,24 @@ $(document).ready(function () {
         $("#popup__close").css({ "z-index": "19" });
 
     });
+
+    const buttonsWrapper = document.querySelector(".map");
+    const slides = document.querySelector(".inner");
+    
+    buttonsWrapper.addEventListener("click", e => {
+      if (e.target.nodeName === "BUTTON") {
+        Array.from(buttonsWrapper.children).forEach(item =>
+          item.classList.remove("active")
+        );
+        if (e.target.classList.contains("first")) {
+          slides.style.transform = "translateX(-0%)";
+          e.target.classList.add("active");
+        } else {
+          slides.style.transform = "translateX(-57%)";
+          e.target.classList.add("active");
+        }
+      }
+    });
+    
+
 });
