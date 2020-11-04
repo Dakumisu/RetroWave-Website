d8f7225ae0c9ff30f4b347043c1b9de0650d8038
Dakumisu <56684844+Dakumisu@users.noreply.github.com>
2020-11-04 03:45:52 +0100

    Update cursor.js

--- data/js/cursor.js
+++ data/js/cursor.js
@@ -1,8 +1,7 @@
+let mouseCursor = document.querySelector(".cursor");
+let link = document.querySelectorAll("a, button, .map button, .infos svg, .music--container svg, input, textarea");
+
 window.addEventListener("load", function () {
-
-    let mouseCursor = document.querySelector(".cursor");
-    let link = document.querySelectorAll("a, button, .map button, .infos svg, .music--container svg, input, textarea");
-
     window.addEventListener("mousemove", cursor);
 
     function cursor(e) {
@@ -13,9 +12,27 @@ window.addEventListener("load", function () {
     link.forEach(g => {
         g.addEventListener("mouseover", () => {
             mouseCursor.classList.add("cursor__grow");
-        });
+        })
         g.addEventListener("mouseleave", () => {
             mouseCursor.classList.remove("cursor__grow");
-        });
-    });
-});
+        })
+    })
+})
+
+window.addEventListener("scroll", function () {
+    window.addEventListener("mousemove", cursor);
+
+    function cursor(e) {
+        mouseCursor.style.top = e.pageY + 'px';
+        mouseCursor.style.left = e.pageX + 'px';
+    }
+
+    link.forEach(g => {
+        g.addEventListener("mouseover", () => {
+            mouseCursor.classList.add("cursor__grow");
+        })
+        g.addEventListener("mouseleave", () => {
+            mouseCursor.classList.remove("cursor__grow");
+        })
+    })
+})
